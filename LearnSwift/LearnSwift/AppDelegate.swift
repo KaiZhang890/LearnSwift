8e0a557cf09233f088d2a51adeea2fdd4dd62543
zhangkai <82190688@qq.com>
2015-07-03 16:33:16 +0800

    finish '"A swift tour"

--- LearnSwift/LearnSwift/AppDelegate.swift
+++ LearnSwift/LearnSwift/AppDelegate.swift
@@ -16,6 +16,146 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        println("Hello World")
+        
+        var myVariable = 42
+        myVariable = 50
+        let myConstant = 42
+        
+        let explicitFloat: Float = 4
+        
+        // Values are never implicitly converted to another type.
+        let label = "The width is "
+        let width = 94
+        let widthLabel = label + String(width)
+        
+        let number = 44
+        let total = "Here are \(number) aples."
+        
+        var shoppingList = [String]()
+        var occupations = [String: String]()
+        //shoppingList[0] = "Mi TV"
+        occupations["kai"] = "programmer"
+        
+        let scores = [75, 43, 103, 87, 12]
+        var teamScore = 0
+        for score in scores {
+            if score > 50 {
+                teamScore += 3
+            } else {
+                teamScore += 1
+            }
+        }
+        println(teamScore)
+        
+        var optionalName: String? = "John Appleseed"
+        optionalName = nil
+        var greeting = "Hello"
+        if let name = optionalName {
+            greeting = "Hello, \(name)"
+        }
+        println(greeting)
+        
+        let vegetable = "red pepper"
+        switch vegetable {
+        case "celery":
+            let vegetableComment = "Add some raisins and make ants on a log."
+        case "cucumber", "watercress":
+            let vegetableComment = "That would make a good tea sandwich."
+        case let x where x.hasSuffix("pepper"):
+            let vegetableComment = "Is it a spicy \(x)?"
+        default:
+            let vegetableComment = "Everything tastes good in soup."
+        }
+        
+        let interestingNumbers = [
+            "Pirme": [2, 3, 5, 7, 11, 13],
+            "Fibonacci": [1, 1, 2, 3, 5, 8],
+            "Square": [1, 4, 9, 16, 25],
+        ]
+        var largest = 0
+        var largestKind = ""
+        for (kind, numbers) in interestingNumbers {
+            for number in numbers {
+                if number > largest {
+                    largest = number
+                    largestKind = kind
+                }
+            }
+        }
+        println("\(largest) - \(largestKind)")
+        
+        
+        var firstForLoop = 0
+        for i in 0..<4 {
+            firstForLoop += i
+        }
+        println(firstForLoop)
+        
+        println(greet("Bob", day: "Tuesday"))
+        
+        let statistics = calculateStatistics([5, 3, 100, 3, 9])
+        println(statistics.2)
+        println(statistics.sum)
+        
+        println(sumOf(1, 2, 3))
+        
+        func returnFifteen() -> Int {
+            var y = 10
+            
+            func add() {
+                y += 5
+            }
+            
+            add()
+            return y
+        }
+        println(returnFifteen())
+        
+        // Functions are a first-class type.
+        func makeIncrementer() -> (Int -> Int) {
+            func addOne(number: Int) -> Int {
+                return 1 + number
+            }
+            return addOne
+        }
+        var increment = makeIncrementer()
+        println(increment(8))
+        
+        func hasAnyMatches(list: [Int], condition: Int -> Bool) -> Bool {
+            for item in list {
+                if condition(item) {
+                    return true
+                }
+            }
+            
+            return false
+        }
+        func lessThanTen(number: Int) -> Bool {
+            return number < 10
+        }
+        var numbers = [20, 19, 17, 12, 30]
+        if hasAnyMatches(numbers, lessThanTen) {
+            println("has matches")
+        } else {
+            println("no matches")
+        }
+        
+        numbers.map({
+            (number: Int) -> Int in
+            let result = 3 * number
+            return result;
+        })
+        
+        let mappedNumbers = numbers.map({
+            (number: Int) -> Int in
+            return number % 2 == 0 ? 0 : number
+        })
+        println(mappedNumbers)
+        
+        numbers.sort { $0 > $1 }
+        println(numbers)
+        
         return true
     }
 
@@ -41,6 +181,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    // MARK: my functions
+    func greet(name: String, day: String) -> String {
+        return "Hello \(name), today is \(day)."
+    }
+    // int scores[], scores: [Int]
+    func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+        var min = scores[0]
+        var max = scores[0]
+        var sum = 0
+        
+        for score in scores {
+            if score > max {
+                max = score
+            } else if score < min {
+                min = score
+            }
+            
+            sum += score
+        }
+        
+        return (min, max, sum)
+    }
+    
+    func sumOf(numbers: Int...) -> Int {
+        var sum = 0
+        for number in numbers {
+            sum += number
+        }
+        return sum
+    }
 }
 
--- LearnSwift/LearnSwift/ViewController.swift
+++ LearnSwift/LearnSwift/ViewController.swift
@@ -8,11 +8,279 @@
 
 import UIKit
 
+class NamedShape {
+    var numberOfSides = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simipleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simipleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+
+class Circle: NamedShape {
+    var radius: Double
+    
+    init(radius: Double, name: String) {
+        self.radius = radius
+        super.init(name: name)
+        numberOfSides = 1
+    }
+    
+    func area() -> Double {
+        return M_PI * radius * radius
+    }
+    
+    override func simipleDescription() -> String {
+        return "A circle with radius of length \(radius)"
+    }
+}
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simipleDescription() -> String {
+        return "An equilaterial triangle with sides of length \(sideLength)."
+    }
+}
+
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+enum Rank: Int {
+    case Ace = 1
+    case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+    case Jack, Queen, King
+    func simpleDescription() -> String {
+        switch self {
+        case .Ace:
+            return "ace"
+        case .Jack:
+            return "jack"
+        case .Queen:
+            return "queen"
+        case .King:
+            return "king"
+        default:
+            
+            return String(self.rawValue)
+        }
+    }
+    
+    func isBigger(otherRank: Rank) -> Bool {
+        return self.rawValue > otherRank.rawValue
+    }
+    
+    static let allValues = [Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King]
+}
+
+enum Suit {
+    case Spades, Hearts, Diamonds, Clubs
+    func simpleDescription() -> String {
+        switch self {
+        case .Spades:
+            return "spades"
+        case .Hearts:
+            return "hearts"
+        case .Diamonds:
+            return "diamonds"
+        case .Clubs:
+            return "clubs"
+        }
+    }
+    
+    func color() -> String {
+        switch self {
+        case .Diamonds, .Hearts:
+            return "red"
+        case .Spades, .Clubs:
+            return "black"
+        }
+    }
+    
+    static let allValues = [Spades, Hearts, Diamonds, Clubs]
+}
+
+// one of the most important differences between structures and classes is that structures are always copied when they are passed around in your code, but classes are passed by reference
+struct Card {
+    var rank: Rank
+    var suit: Suit
+    func simpleDescription() -> String {
+        return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+    }
+    
+    static func createDeck() -> [Card] {
+        var cards = [Card]()
+        
+        for rank in Rank.allValues {
+            for suit in Suit.allValues {
+                var card = Card(rank: rank, suit: suit)
+                cards.append(card)
+            }
+        }
+        
+        return cards
+    }
+}
+
+enum ServerResponse {
+    case Result(String, String)
+    case Error(String)
+}
+
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+    func adjust() {
+        simpleDescription += " Now 100% adjusted."
+    }
+}
+
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+
+enum SimpleEnumeration: ExampleProtocol {
+    case Base, Adjusted
+    
+    func getDescription() -> String {
+        switch self {
+        case Base:
+            return "Base description of enum"
+        case Adjusted:
+            return "Adjusted description of enum"
+        }
+    }
+    
+    var simpleDescription: String {
+        get {
+            return self.getDescription()
+        }
+    }
+    
+    mutating func adjust() {
+        self = SimpleEnumeration.Adjusted
+    }
+}
+
+extension Double {
+    var absoluteValue: Double {
+        return abs(self)
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var shape = NamedShape(name: "No name")
+        shape.numberOfSides = 7
+        println(shape.simipleDescription())
+        
+        let circle = Circle(radius: 2, name: "my circle")
+        println(circle.area())
+        println(circle.simipleDescription())
+        
+        var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+        println(triangle.perimeter)
+        triangle.perimeter = 9.9
+        println(triangle.sideLength)
+        
+        var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+        println(triangleAndSquare.square.sideLength)
+        println(triangleAndSquare.triangle.sideLength)
+        triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+        println(triangleAndSquare.triangle.sideLength)
+        
+        let ace = Rank.Ace
+        println("\(ace.simpleDescription()) \(ace.rawValue)")
+        println(Rank.Queen.isBigger(Rank.King) ? "is bigger" : "not bigger")
+        
+        if let convertRank = Rank(rawValue: 3) {
+            println(convertRank.simpleDescription())
+        }
+        
+        println(Suit.Hearts.color())
+        
+        let threeOfSpades = Card(rank: .Three, suit: .Spades)
+        println(threeOfSpades.simpleDescription())
+        
+        for card in Card.createDeck() {
+            println(card.simpleDescription())
+        }
+        
+        let success = ServerResponse.Result("6:00 am", "8:09 pm")
+        let failure = ServerResponse.Error("Out of cheese.")
+        switch success {
+        case let .Result(sunrise, sunset):
+            println("Sunrise is at \(sunrise) and sunset is at \(sunset)")
+        case let .Error(error):
+            println("Failure... \(error)")
+        }
+        
+        println((-12.2).absoluteValue)
     }
 
     override func didReceiveMemoryWarning() {
